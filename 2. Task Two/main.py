8c8e64862b0f75d9b0ea4037f8722e8b04fc051e
kolaisaac <blestisaac10@gmail.com>
2023-09-11 15:39:33 +0100

    main for task two :fire: :rocket:

---
+++ 2. Task Two/main.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(120))
+
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
